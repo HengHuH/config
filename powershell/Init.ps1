0a05fe981cc38a1eb67bb513b7ff22c653f71505
huangxing03 <huangxing03@corp.netease.com>
2021-03-03 19:00:25 +0800

    init

--- powershell/Init.ps1
+++ powershell/Init.ps1
@@ -1,5 +1,57 @@
 # Heng's Profile for PowerShell7
 
+#------------------------------- Import Modules BEGIN -------------------------------
+
+Import-Module PSReadLine
+
+#------------------------------- Import Modules END   -------------------------------
+
+
+#-------------------------------  Set Hot-keys BEGIN  -------------------------------
+# 设置 Tab 键补全
+# Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+# 设置 Ctrl+d 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+
+# 设置 Ctrl+d 为退出 PowerShell
+Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
+
+# 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+
+# 设置向上键为后向搜索历史记录
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+
+# 设置向下键为前向搜索历史纪录
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+#-------------------------------  Set Hot-keys END    -------------------------------
+
+
+
+#-------------------------------    Functions BEGIN   -------------------------------
+# Python 直接执行
+# $env:PATHEXT += ";.py"
+
+# 更新 pip 的方法
+function Update-Packages {
+    # update pip
+    Write-Host "Step 1: 更新 pip" -ForegroundColor Magenta -BackgroundColor Cyan
+    $a = pip list --outdated
+    $num_package = $a.Length - 2
+    for ($i = 0; $i -lt $num_package; $i++) {
+        $tmp = ($a[2 + $i].Split(" "))[0]
+        pip install -U $tmp
+    }
+
+    # update TeX Live
+    $CurrentYear = Get-Date -Format yyyy
+    Write-Host "Step 2: 更新 TeX Live" $CurrentYear -ForegroundColor Magenta -BackgroundColor Cyan
+    tlmgr update --self
+    tlmgr update --all
+}
+
+#-------------------------------    Functions END     -------------------------------
 # Prompt
 
 function prompt {
